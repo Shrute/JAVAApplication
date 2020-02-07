c947330b9f0eae9036ce6e015284add11227a987
ds <ds@192.168.0.9>
2020-02-07 10:04:04 +0530

    First commit

---
+++ JAVA APPlication/src/basic.java
@@ -0,0 +1,19 @@
+
+public class basic {
+
+	public static void main(String[] args) {
+		int number=10;
+		double no=23.33;
+		String name="shruthi";
+		String word="hello";
+		String fullword= name+word;		
+		System.out.println(number );
+		System.out.println(name);
+		System.out.println("hello"+ " " +"shruthi");
+		System.out.println(fullword);
+		System.out.println("double number");
+		
+		
+	}
+
+}
---
+++ JAVA APPlication/src/loops.java
@@ -0,0 +1,14 @@
+
+public class loops {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while(a <= 10)
+		{
+			System.out.println("shruthi" + a);
+			a++;
+			
+		}
+	}
+
+}
