c3ac72d15b949bbecd95468db39fbb046a9b75d4
ds <ds@172.16.5.22>
2020-02-14 11:26:44 +0530

    input from user

--- JAVA APPlication/src/forloop.java
+++ JAVA APPlication/src/forloop.java
@@ -4,7 +4,7 @@ public class forloop {
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++)
 		{
-			System.out.println("lets count"+i);
+			System.out.println("lets count"+" " +i);
 		}
 	}
 
---
+++ JAVA APPlication/src/ifloop.java
@@ -0,0 +1,25 @@
+
+public class ifloop {
+
+	public static void main(String[] args) {
+		String s = "name";
+		if(4>5) {
+			System.out.println("false");
+		}
+			else if(s=="name"){
+				System.out.println("true");
+			}
+			else {
+				System.out.println("tru");
+			}
+			
+			}
+		
+		}
+		
+	
+	
+
+	
+
+
