8b7b5cfb15dc9fccfcd121d71e88689b62bb16f8
ds <ds@172.16.5.18>
2020-02-18 14:59:08 +0530

    class object

---
+++ JAVA APPlication/src/example.java
@@ -0,0 +1,13 @@
+class books{
+	String subject1 = "science";
+	String subject2 ="maths";
+}
+public class example {
+
+	public static void main(String[] args) {
+		books b1=new books();
+		System.out.println("my fav "+b1.subject2);
+
+	}
+
+}
